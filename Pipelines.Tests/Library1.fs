2a60712b6d8d1f89eaa7173a03fd97ef147eda2e
Bill Dean <tsigoloahc@gmail.com>
2017-10-15 10:31:42 -0400

    Add project files.

---
+++ Pipelines.Tests/Library1.fs
@@ -0,0 +1,135 @@
+﻿namespace Pipeline.Tests
+open Xunit
+open Pipelines
+open Pipelines.Pipeline
+open Pipelines.Attempt
+
+type Class1() = 
+    [<Fact>]
+    member this.TrivialTest () = 
+        ()
+
+    [<Fact>]
+    member this.ZeroStepTest () = 
+        let pipeline = new PipelineBuilder()
+
+        let d s =pipeline {   
+                    return s
+                }
+        
+        let att=d 3
+        let r = att()
+        Xunit.Assert.Equal(Success(3),r)
+        ()
+
+    [<Fact>]
+    member this.MultiStepTest () = 
+        let pipeline = new PipelineBuilder()
+        let add1 x = x + 1
+
+        let d (s:int) =pipeline { 
+                    let! f = s >?> add1
+                    let! g = f >?> add1 
+                    let! h = g >?> add1 
+                    let ddd = 1
+                    return h
+                }
+        
+        let att=d 3
+        let r = att()
+        Xunit.Assert.Equal(Success(6),r)
+        ()
+
+    [<Fact>]
+    member this.MultiStepFailTest () = 
+        let pipeline = new Pipeline.PipelineBuilder()
+        let mutable count = 0;
+        let add1 x = 
+                count<-count+1
+                x + 1
+        let ex = new System.Exception();
+        let iThrow x = raise ex
+
+        let d (s:int) =pipeline { 
+                    let! f = s >?> add1 
+                    let! g = f >?> iThrow 
+                    let! h = g >?> add1 
+
+                    return h
+                }
+        
+        let att=d 3
+        let r = att ()
+        Xunit.Assert.Equal(Failure(ex),r)
+        Xunit.Assert.Equal(1,count)
+        ()
+
+    [<Fact>]
+    member this.MultiStepDelayTest () = 
+        let pipeline = new Pipeline.PipelineBuilder()
+        let mutable count = 0;
+        let add1 x = 
+                count<-count+1
+                x + 1
+
+        let d (s:int) =pipeline { 
+                    let! f = s >?> add1 
+                    let! g = f >?> add1 
+                    let! h = g >?> add1 
+
+                    return h
+                }
+
+        Xunit.Assert.Equal(0,count)        
+        let att=d 3
+        Xunit.Assert.Equal(0,count)        
+        let r = att()
+        Xunit.Assert.Equal(3,count)
+        ()
+
+
+    [<Fact>]
+    member this.OneStepTest () = 
+        let pipeline = new Pipeline.PipelineBuilder()
+        let add1 x = x + 1
+
+        let d (s:int) =pipeline { 
+                    let! f = s >?> add1 
+                    return f
+                }
+        
+        let att=d 3
+        let r =  att()
+        Xunit.Assert.Equal(Success(4),r)
+        ()
+
+
+    [<Fact>]
+    member this.FailTest () = 
+        let pipeline = new Pipeline.PipelineBuilder()
+        let ex = new System.Exception();
+        let add1 x = raise ex
+
+        let d (s:int) =pipeline { 
+                    let! f = s >?> add1 
+                    return f
+                }
+        
+        let att=d 3
+        let r = att()
+        Xunit.Assert.Equal(Failure(ex),r)
+        ()
+    
+    [<Fact>]
+    member this.ReturnDirect () = 
+        let pipeline = new Pipeline.PipelineBuilder()
+
+        let d (s:int) =pipeline { 
+                    return! fun ()->Success(s)
+                }
+        
+        let att=d 3
+        let r = att()
+        Xunit.Assert.Equal(Success(3),r)
+        ()
+
---
+++ Pipelines/Pipelines.Pipelines.fs
@@ -0,0 +1,43 @@
+﻿namespace Pipelines
+
+type StepResult<'TResult,'TError> =
+        | Success of 'TResult
+        | Failure of 'TError
+
+type Attempt<'TResult> = (unit->StepResult<'TResult,System.Exception>)
+    
+module Attempt=
+    let TryCatch handler f x =
+        try
+            Success (f x)
+        with (ex)->
+            Failure (ex |> handler)
+
+    let SimpleTryCatch f x = TryCatch (fun x->x) f x
+
+    let Attempt f x =
+        fun () -> SimpleTryCatch f x
+
+    let (>?>) x f =
+        Attempt f x
+    
+
+module Pipeline=
+
+    let succeed x =(fun()->Success(x))
+    let fail ex = (fun()->Failure(ex))
+    let runAttempt (a:Attempt<'T>) = a()
+    let bind p rest = match runAttempt p with Failure ex -> fail ex | Success r -> (rest r)
+    let delay f = (fun () -> runAttempt(f()))
+    let combine p1 p2 = (fun () -> match p1() with Failure(e)-> p2() | res->res)
+
+    type PipelineBuilder() = 
+            member __.Bind (p,rest) = bind p rest
+            member __.Delay (f) =delay f
+            member __.Return (x) =  succeed x
+            member __.ReturnFrom (x:Attempt<'TResult>) = x
+            member __.Combine (p1:Attempt<'TResult>,p2:Attempt<'TResult>) = combine p1 p2
+            member __.Zero () = fail (new System.Exception("No Work Specifiec"))
+
+
+
